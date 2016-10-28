9d1828fa203a9239570a65e1ed2f19d37627aa5f
hcheng.bbboy <arvin@ipfan.com>
2016-10-28 14:05:46 +0800

    angular http study

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,13 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+// import {FormsModule}   from '@angular/forms';
+import {HttpModule}    from '@angular/http';
 import {AppComponent} from "./app.component/app.component";
+import {HttpComponent} from "./app.http/app.http";
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
+    imports: [BrowserModule,HttpModule],
+    declarations: [AppComponent, HttpComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {
